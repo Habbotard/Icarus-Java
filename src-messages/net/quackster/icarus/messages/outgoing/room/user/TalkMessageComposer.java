1b389fecb7d3c95caca82875e1454cb95cd10e88
Alex Miller <quacksterr.15@gmail.com>
2015-12-11 10:48:50 +1000

    Stopped rotating head if player is walking

--- src-messages/net/quackster/icarus/messages/outgoing/room/user/TalkMessageComposer.java
+++ src-messages/net/quackster/icarus/messages/outgoing/room/user/TalkMessageComposer.java
@@ -13,11 +13,10 @@ import net.quackster.icarus.game.room.models.Rotation;
 
 public class TalkMessageComposer extends Response {
 
-	
 	public TalkMessageComposer(RoomUser roomUser, boolean shout, String message, int count, int textColour) {
 
 		List<Integer> allowedColours = new ArrayList<Integer>(Arrays.asList(new Integer[] { 0, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20, 29 }));
-		
+
 		if (!allowedColours.contains(textColour)) {
 			textColour = 0;
 		}
@@ -30,24 +29,23 @@ public class TalkMessageComposer extends Response {
 
 		// TODO: Filtering/anti-flodding
 		// TODO: Chat logs
-		
+
 		// TODO: If bot then chat colour = 2
-		
-        this.init(header);
-        this.appendInt32(roomUser.getVirtualId());
-        this.appendString(message);
-        this.appendInt32(0);
-        this.appendInt32(textColour);
-        this.appendInt32(0);// links count (foreach string string bool)
-        this.appendInt32(count);
-        
-        Point point = roomUser.getPoint();
-        
-        int head = Rotation.calculate(point.getX(), point.getY(), point.getX(), point.getY());
-        
-        roomUser.setRotation(head, true);
-        roomUser.updateStatus();
-		
+
+		this.init(header);
+		this.appendInt32(roomUser.getVirtualId());
+		this.appendString(message);
+		this.appendInt32(0);
+		this.appendInt32(textColour);
+		this.appendInt32(0);// links count (foreach string string bool)
+		this.appendInt32(count);
+
+		if (!roomUser.isWalking()) {
+			Point point = roomUser.getPoint();
+			roomUser.setRotation(Rotation.calculate(point.getX(), point.getY(), point.getX(), point.getY()), true);
+			roomUser.updateStatus();
+		}
+
 	}
 
 }
